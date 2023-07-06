795df12f0f1954f7b25b8e8210d6bdd7eca94aea
Ing-waz <farrell.j@protonmail.com>
2023-07-06 13:52:21 -0600

    added knex requests to server.js

--- server/knexfile.js
+++ server/knexfile.js
@@ -12,10 +12,14 @@ const config = {
   connection: {
     host: process.env.PG_HOST || 'localhost',
     port: process.env.PG_PORT || 5432,
-    user: process.env.PG_USER || 'learn-user',
+    user: process.env.PG_USER || 'Ingwaz',
     password: process.env.PG_PASSWORD,
-    database: process.env.PG_DATABASE || 'learn-db',
+    database: process.env.PG_DATABASE || 'tankvana_db',
   },
+
+  seeds: {
+    directory: './seeds'
+  }
 };
 
 export default config;
--- server/src/server.js
+++ server/src/server.js
@@ -5,6 +5,7 @@
  */
 import app from './app.js';
 import http from 'http';
+const knex = require('knex')(require('../knexfile.js')["development"])
 
 /**
  * Get port from environment and store in Express.
@@ -52,4 +53,65 @@ process.on('SIGTERM', () => {
   server.close(() => {
     console.debug('HTTP server closed.');
   });
+});
+
+app.get('/tank_inventory', (req, res) => {
+  knex('tank_inventory')
+      .select('*')
+      .then(tankvana_inventory => {
+          res.json(tankvana_inventory); 
+      })
+})
+
+app.get('/rentals', (req, res) => {
+  knex('rentals')
+      .select('*')
+      .then(rentals => {
+          res.json(rentals); 
+      })
+})
+
+app.get('/customers', (req, res) => {
+  knex('customers')
+      .select('*')
+      .then(customers => {
+          res.json(customers); 
+      })
+})
+
+app.get('/vehicle_loadouts', (req, res) => {
+  knex('vehicle_loadouts')
+      .select('*')
+      .then(vehicle_loadouts => {
+          res.json(vehicle_loadouts); 
+      })
+})
+
+app.get('/weapon_inventory', (req, res) => {
+  knex('locations')
+      .select('*')
+      .then(locations => {
+          res.json(locations); 
+      })
+})
+
+app.get('/locations', (req, res) => {
+  knex('locations')
+      .select('*')
+      .then(locations => {
+          res.json(locations); 
+      })
+})
+
+app.get('/weapon_inventory', (req, res) => {
+  knex.select('*')
+      .from('weapon_inventory')
+      .join('weapon_inventory', 'vehicle_loadouts.loadout_id', '=', 'inventory_id.id')
+      .then(data => {
+          res.json(data);
+      })
+      .catch(err => {
+          console.error(err);
+          res.status(500).json({ message: 'An error occurred' });
+      });
 });
