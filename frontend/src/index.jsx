2c05bb3e66e3c7dfcd1ed48fa6529e0c92a27ae9
devwags <devwags@gmail.com>
2023-07-06 13:23:15 -0600

    initial commit to sign_in branch

--- frontend/src/index.jsx
+++ frontend/src/index.jsx
@@ -4,6 +4,7 @@ import ReactDOM from 'react-dom/client';
 import './scss/index.scss';
 import App from './components/App/App';
 
+// Entry Point
 const root = ReactDOM.createRoot(
   document.getElementById('root'),
 );
